785d50a8c750d5ac6900f0aea231b270ae724e36
Jonathan Fuchs <j.fuchs@data-team.de>
2015-03-19 16:32:28 +0100

    # Prüfung ob NUmeric

---
+++ src/BigDecimalTest.java
@@ -0,0 +1,62 @@
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BigDecimalTest {
+
+    private List<BigDecimal> bigDecimalList;
+
+    private List<String> stringList;
+
+    private String valueOne = "1.000,5";
+
+    private String valueTwo = "10,60";
+
+    private String valueThree = "5,06";
+
+    @Before
+    public void setup() {
+        bigDecimalList = new ArrayList<>();
+        stringList = new ArrayList<String>();
+        stringList.add(valueOne);
+        stringList.add(valueTwo);
+        stringList.add(valueThree);
+    }
+
+    @Test
+    public void testScale() throws ParseException {
+        prepareForScale();
+        Collections.sort(bigDecimalList);
+        for (int i = 0; i < bigDecimalList.size(); i++) {
+            assertEquals(stringList.get(i), bigDecimalList.get(i).toString());
+        }
+    }
+
+    private void prepareForScale() throws ParseException {
+        for (int i = 0; i < stringList.size(); i++) {
+            String value = stringList.get(i);
+            String tempValue = value.replace(",", "");
+            tempValue = tempValue.replace(".", "");
+            if (NumberUtils.isNumber(tempValue)) {
+                DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(Locale.GERMANY);
+                df.setParseBigDecimal(true);
+                BigDecimal bd = (BigDecimal) df.parseObject(value);
+                bigDecimalList.add(bd.setScale(2));
+            } else {
+                System.out.println("Die Zahl {" + value + "} ist kein BigDecimal ");
+                break;
+            }
+        }
+    }
+}
